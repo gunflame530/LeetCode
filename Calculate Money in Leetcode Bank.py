9ce1ef9549a89f460710270214329fcef7d876d8
Nebula 530 <yuexiong_530@outlook.com>
2021-01-19 16:53:12 +0800

    1716. 计算力扣银行的钱

---
+++ Calculate Money in Leetcode Bank.py
@@ -0,0 +1,36 @@
+Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+Hercy 想要为购买第一辆车存钱。他 每天 都往力扣银行里存钱。
+最开始，他在周一的时候存入 1 块钱。从周二到周日，他每天都比前一天多存入 1 块钱。在接下来每一个周一，他都会比 前一个周一 多存入 1 块钱。
+给你 n ，请你返回在第 n 天结束的时候他在力扣银行总共存了多少块钱。
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 10
+Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+Example 2:
+
+Input: n = 10
+Output: 37
+Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+Example 3:
+
+Input: n = 20
+Output: 96
+Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+
+Answer:
+
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        weeks = math.floor(n/7) + 1
+        days = n % 7
+        money = 0
+        for i in range(weeks):
+            for j in range(days):
+                money = money + i + j + 1
+        return money
