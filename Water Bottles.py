aab48f68a1fd292f942b16290221b5cf4b396a28
Nebula 530 <yuexiong_530@outlook.com>
2021-01-19 18:14:04 +0800

    换酒问题

--- Water Bottles.py
+++ Water Bottles.py
@@ -1,3 +1,4 @@
+"""
 Given numBottles full water bottles, you can exchange numExchange empty water bottles for one full water bottle.
 The operation of drinking a full water bottle turns it into an empty bottle.
 Return the maximum number of water bottles you can drink.
@@ -31,6 +32,7 @@ Constraints:
 2 <= numExchange <= 100
 
 Answer:
+"""
 
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
