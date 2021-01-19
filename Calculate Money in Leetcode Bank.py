1ed335d7027539491d85040a64cf5e37e3044595
Nebula 530 <yuexiong_530@outlook.com>
2021-01-19 18:13:09 +0800

    计算力扣银行的钱

--- Calculate Money in Leetcode Bank.py
+++ Calculate Money in Leetcode Bank.py
@@ -1,3 +1,4 @@
+"""
 Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
 He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
 Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
@@ -24,7 +25,7 @@ Output: 96
 Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
 
 Answer:
-
+"""
 class Solution:
     def totalMoney(self, n: int) -> int:
         weeks = math.floor(n/7)
